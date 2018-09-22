598067f8a24cd23e0b3b5069163787f7fc0aacde
Haniel Huam <hanielhuam@hotmail.com>
2018-09-21 23:29:36 -0300

    listando as tarefas adicionadas na section de todo

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-var menu = ["Projetos"]
-console.log(listItens(menu));
-document.getElementById("menus").innerHTML = listItens(menu);
+'use strict'
+
+let tarefasToDo = [];
+
+function adicionarNovaTarefa() {
+    var tarefa = document.getElementById('nome').value;
+    console.log("qualquer coisa" + tarefa);
+    tarefasToDo.push(tarefa);
+    document.getElementById('todo').innerHTML = listItens(tarefasToDo);
+}
 
 function listItens(itens) {
 
     var listItens = "<ul>";
-    
+    var i
     for(i=0; i<itens.length; i++) {
         listItens += "<li>" + itens[i] + "</li>";
     }
